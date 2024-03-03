bc95439266b0cd4a74b7834651eb8bffd0f6b929
NickAst45 <103696212+NickAst45@users.noreply.github.com>
2024-03-02 23:15:47 -0500

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 // [EN] > Add the number that will be Bot and those that will be owners.
 global.owner = [
 ["5492266466080", '𝙊𝙬𝙣𝙚𝙧 💖🐈', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
-["593963376278"]
+["593963376278"]]
 
 global.mods = []
 global.prems = []
